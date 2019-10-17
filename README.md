51b6ebe75081a09fdd62d4f877b3ffc55b1756f2
michael <zi-peng.song@microfocus.com>
2019-10-17 20:31:20 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# apache-ajp" 
+## apache-ajp 
+ 
+### Run 
+- sudo docker run -p 443:443 -p 8080:80 -e NODES='PPM_MULTINODE:16.186.78.162:25202,PPM_MULTINODE2:16.186.78.162:25302,PPM_MULTINODE3:16.186.77.158:25402,PPM_MULTINODE4:16.186.77.158:25502' -d snzip/apache-ajp
+
+### Parameters
+- NODES
+server_name. ( The name of the Tomcat needs to be equal to the value of the jvmRoute attribute in the Engine element of each Tomcat's server.xml.)
+- IP
+- ajp port
+### Reference
+- https://tomcat.apache.org/connectors-doc/common_howto/loadbalancers.html
+
+### Docker image 
+- https://hub.docker.com/r/snzip/apache-ajp
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#NODES=PPM_MULTINODE:16.186.78.162:25202,PPM_MULTINODE2:16.186.78.162:25302,PPM_MULTINODE3:16.186.77.158:25402,PPM_MULTINODE4:16.186.77.158:25502
+# NODES=PPM_MULTINODE:16.186.78.162:25202,PPM_MULTINODE2:16.186.78.162:25302,PPM_MULTINODE3:16.186.77.158:25402,PPM_MULTINODE4:16.186.77.158:25502
 
 set -e
 
 output='/etc/libapache2-mod-jk/workers.properties'
-#output='workers.properties'
+# output='workers.properties'
  
 rm -f $output
 
@@ -20,9 +20,8 @@ if [ ! -z "$NODES" ]; then
     for i in "${ADDR[@]}"; do
         seq=$((seq+1)) 
         
-        echo worker.${NODE[0]}.type=ajp13 >> $output
-
         IFS=':' read -r -a NODE <<< "$i"
+        echo worker.${NODE[0]}.type=ajp13 >> $output
         echo worker.${NODE[0]}.host=${NODE[1]} >> $output
         echo worker.${NODE[0]}.port=${NODE[2]} >> $output 
  
